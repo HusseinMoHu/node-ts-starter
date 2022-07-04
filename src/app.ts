f58a67382d911d2e511040df040ab0ea15a1b675
husseinmo96 <95226088+husseinmo96@users.noreply.github.com>
2022-07-04 12:10:45 +0200

    added helmet for headers security

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import mainRouter from './main.routes'
-
+import helmet from 'helmet'
 
 const app = express()
 
+app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
